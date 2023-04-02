c31f54b02f472ef1aaa63369a74f86aadca5a445
Suyogya Poudel <soga2058@gmail.com>
2023-04-02 11:09:54 -0600

    Made states component that allows user to select a US state.

--- frontend/src/components/DisplayNews/News.js
+++ frontend/src/components/DisplayNews/News.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './News.css'
+import States from "../States/States";
 
 function News() {
     return (
         <div className="right-container">
-            <div className="temp">Right side needs to be completed.</div>
+            {/* <div className="temp">Right side needs to be completed.</div> */}
+            <States />
         </div>
     )
 }
---
+++ frontend/src/components/States/States.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import './States.css'
+
+function States() {
+    const [selectedState, setSelected] = useState("");
+    const states = [
+        "Alabama",
+        "Alaska",
+        "Arizona",
+        "Arkansas",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "Delaware",
+        "Florida",
+        "Georgia",
+        "Hawaii",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Maine",
+        "Maryland",
+        "Massachusetts",
+        "Michigan",
+        "Minnesota",
+        "Mississippi",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+        "North Dakota",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "Utah",
+        "Vermont",
+        "Virginia",
+        "Washington",
+        "West Virginia",
+        "Wisconsin",
+        "Wyoming"
+    ];
+
+    const changeState = (e) => {
+        setSelected(e.target.value);
+    }
+    
+      
+    return (
+        <div className="states">
+           <select className="selectOptions" value={selectedState} onChange={changeState}>
+            <option>Select a state</option>
+            {states.map((state) => (
+                <option key={state} value={state}>
+                    {state}
+                </option>
+            ))}
+           </select>
+        </div>
+    );
+}
+
+export default States;
