dc10e0ad6ce0cff75953013b2c194cef84750296
Suyogya Poudel <soga2058@gmail.com>
2023-04-11 12:48:21 -0600

    removed some unnecessary lines.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -10,32 +10,3 @@ const store = configureStore({
 });
 
 export default store;
-
-
-/* How to use redux for state management
-
-// import useSelector and useDispatch hook
-// useDispatch is not required if you don't change the state on a given component
-import { useSelector, useDispatch } from 'react-redux'
-
-// Import the store and action creators
-// make sure the path is correct
-import store from "../../../store/store";
-import { changeState } from "../../../store/stateSlice";
-
-// Get the currentState from the store using useSelector
-const state = useSelector((state) => state.currentState.value);
-
-// Use dispatch to change the state (currentState)
-const dispatch = useDispatch();
-
-// Example of changing state in a dropdown menu
-const handleChangeState = (e) => {
-  const value = e.target.value;
-  if (value) {
-    dispatch(changeState(value));
-  }
-}
-
-
-*/
